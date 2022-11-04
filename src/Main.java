ded31f0ee64f7c53fcd37911a252f793ab3f48d1
asadev1411 <kaisar1990@mail.ru>
2022-11-04 10:09:46 +0500

    Stopped at HW3 Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
 public class Main {
     public static void main(String[] args) {
 
+        //HW1. Task №1
+        System.out.println("ДЗ-1. Задание №1");
+        for (int q = 1; q <= 10; q++) {
+            System.out.println(q);
+        }
 
+        //HW1. Task №2
+        System.out.println("");
+        System.out.println("ДЗ-1. Задание №2");
+        for (int q = 10; q >= 1; q--) {
+            System.out.println(q);
+        }
+
+        //HW1. Task №3
+        System.out.println("");
+        System.out.println("ДЗ-1. Задание №3");
+        for (int q = 0+2; q < 17; q+=2) {
+            System.out.println(q);
+        }
+
+        //HW1. Task №4
+        System.out.println("");
+        System.out.println("ДЗ-1. Задание №4");
+        for (int q = 10; q >= -10; q--) {
+            System.out.println(q);
+        }
+
+        //HW2. Task №1
+        System.out.println("");
+        System.out.println("ДЗ-2. Задание №1");
+        for (int y = 1904; y <= 2096; y = y + 4) {
+            if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0);
+            System.out.println(y + " год является високосным.");
+        }
+
+        //HW2. Task №2
+        System.out.println("");
+        System.out.println("ДЗ-2. Задание №2");
+        for (int q = 7; q <= 98; q+=7) {
+            System.out.println(q);
+        }
+
+        //HW2. Task №3
+        System.out.println("");
+        System.out.println("ДЗ-2. Задание №3");
+        for (int q = 1; q <= 512; q*=2) {
+            System.out.println(q);
+        }
     }
 }
